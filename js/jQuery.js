baacd1191fd035343d61fbe4a6ab7eb9b1486fd7
tphvtr <otets.odnako@gmail.com>
2017-08-17 14:17:25 +0300

    pravki

--- js/jQuery.js
+++ js/jQuery.js
@@ -54,6 +54,34 @@ $(document).ready(function() {
      });
 });
 
+$(document).ready(function() {
+    var overlay = $('#overlay'); 
+    var open_modal = $('.open_modal_2'); 
+    var close = $('.modal_close, #overlay'); 
+    var modal = $('.modal_div'); 
+     open_modal.click( function(event){
+         event.preventDefault(); 
+         var div = $(this).attr('href');
+         console.log(div);
+         overlay.fadeIn(400, 
+             function(){ 
+                 $(div)
+                     .css('display', 'block') 
+                     .animate({opacity: 1, top: '50%'}, 200); 
+         });
+     });
+
+     close.click( function(){ 
+            modal
+             .animate({opacity: 0, top: '45%'}, 200, 
+                 function(){ 
+                     $(this).css('display', 'none');
+                     overlay.fadeOut(400);
+                 }
+             );
+     });
+});
+
 $( "#show" ).click(function() {
   $( "div#item" ).toggleClass( "main_show");
 });
